4f008422b12380951877f6b92cc8dcb1af3ef566
masuda <masuda@moonmile.net>
2017-03-24 17:48:00 +0900

    forms: MainViewModel を追加

--- src/SampleTodoXForms/SampleTodoXForms/SampleTodoXForms/Views/MainPage.xaml.cs
+++ src/SampleTodoXForms/SampleTodoXForms/SampleTodoXForms/Views/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using SampleTodoXForms.Models;
+using SampleTodoXForms.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -23,83 +24,20 @@ namespace SampleTodoXForms.Views
             lst.Add(new ToDo() { Id = 1, Text = "item no.1", DueDate = new DateTime(2017, 5, 1), CreatedAt = new DateTime(2017, 3, 1) });
             lst.Add(new ToDo() { Id = 2, Text = "item no.2", DueDate = new DateTime(2017, 5, 3), CreatedAt = new DateTime(2017, 3, 2) });
             lst.Add(new ToDo() { Id = 3, Text = "item no.3", DueDate = new DateTime(2017, 5, 2), CreatedAt = new DateTime(2017, 3, 3) });
-            items = new ToDoFiltableCollection(lst);
-            this.listView.ItemsSource = items;
+            // this.listView.ItemsSource = items = new ToDoFiltableCollection(lst);
+
+            viewModel = new MainViewModel();
+            viewModel.Items = items = new ToDoFiltableCollection(lst);
+            this.BindingContext = viewModel;
+
         }
 
-        /// <summary>
-        /// フィルターができるコレクションクラス
-        /// </summary>
-        class ToDoFiltableCollection : ObservableCollection<ToDo>
-        {
-            // 元のリストデータ
-            private List<ToDo> _items;
-            // ソート用の項目
-            bool _dispComplted = true;
-            int _sortOrder = 0;
-
-            public ToDoFiltableCollection( List<ToDo> items ) : base(items)
-            {
-                _items = items;
-            }
-            public new void Add( ToDo item )
-            {
-                _items.Add(item);
-                SetFilter(_dispComplted, _sortOrder);
-            }
-            public new bool Remove( ToDo item )
-            {
-                bool b = _items.Remove(item);
-                this.Remove(item);
-                return b;
-            }
-            public new void Insert( int index, ToDo item )
-            {
-                _items.Insert(index, item);
-                SetFilter(_dispComplted, _sortOrder);
-            }
-
-            /// <summary>
-            /// 項目のアップデート
-            /// </summary>
-            public void Update()
-            {
-                // ソートを反映させる
-                SetFilter(_dispComplted, _sortOrder);
-            }
-
-            public void SetFilter( bool dispCompleted, int sortOrder )
-            {
-                _dispComplted = dispCompleted;
-                _sortOrder = sortOrder;
-
-                List<ToDo> lst = _items;
-                switch ( sortOrder )
-                {
-                    case 0: // 作成日順/ID順
-                        lst = _items.OrderByDescending(x => x.CreatedAt).ToList();
-                        break;
-                    case 1: // 項目名順
-                        lst = _items.OrderBy(x => x.Text).ToList();
-                        break;
-                    case 2: // 期日順
-                        lst = _items.OrderBy(x => x.DueDate).ToList();
-                        break;
-                }
-                // 未完了だけを表示する
-                if (dispCompleted == false)
-                {
-                    lst = lst.Where(x => x.Completed == false).ToList();
-                }
-                // 全てを追加し直す
-                this.Clear();
-                lst.All(x => { base.Add(x); return true; });
-            }
-        }
 
         // 表示するデータ
         // ObservableCollection<ToDo> items;
         ToDoFiltableCollection items;
+        MainViewModel viewModel;
+
         // 設定
         Setting setting = new Setting()
         {
