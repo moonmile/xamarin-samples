27689f3bbe0a8143aeed30a6a37878ec3da0efcd
masuda <masuda@moonmile.net>
2017-04-17 23:04:39 +0900

    forms for mac を試しに追加

---
+++ src/SampleTodo.Forms/SampleTodo.Forms/SampleTodo.FormsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace SampleTodo.Forms
+{
+	public partial class SampleTodo_FormsPage : ContentPage
+	{
+		public SampleTodo_FormsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
